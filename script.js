4cfc0c41769b88bfb4b3d050deb30ef4e06edf27
Pedro Lacerda <pedrolacerda804@gmail.com>
2023-09-04 12:57:15 -0300

    Explanatory comments

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const tabMenu = document.querySelectorAll('.js-tab-menu li');
 const tabContent = document.querySelectorAll('.js-tab-content section');
 
 function activeTab (index) {
+    // Remove all class like "ativo"
     tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+        section.classList.remove('active');
     })
 
-    tabContent[index].classList.add('ativo');
+    // Add the "active" class to the respective clicked index
+    tabContent[index].classList.add('active');
 }
 
 tabMenu.forEach((itemMenu, index) => {
+    // We create an arrow function with callback in the event and pass the index stored in forEach
     itemMenu.addEventListener('click', () => {
         activeTab(index);
     });
