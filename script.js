92c8480e92543503556a5b19eadacbda5ad35e90
Pedro Lacerda <pedrolacerda804@gmail.com>
2023-09-04 16:05:04 -0300

    Fixed problem, method lenght removed

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const tabMenu = document.querySelectorAll('.js-tab-menu li');
 const tabContent = document.querySelectorAll('.js-tab-content section');
 
-if (tabMenu.lenght && tabContent.lenght) {
-    tabContent[0].classList.add('active');
+tabContent[0].classList.add('active');
 
+if (tabMenu && tabContent) {
     function activeTab (index) {
-        // Remove all class like "active"
-        tabContent.forEach((section) => {
-            section.classList.remove('active');
+        tabContent.forEach((item) => {
+            item.classList.remove('active');
         })
     
-        // Add the "active" class to the respective clicked index
         tabContent[index].classList.add('active');
     }
     
-    tabMenu.forEach((itemMenu, index) => {
-        // We create an arrow function with callback in the event and pass the index stored in forEach
-        itemMenu.addEventListener('click', () => {
+    tabMenu.forEach((item, index) => {
+        item.addEventListener('click', () => {
             activeTab(index);
-        });
+        })
     })
 }
