4e11dcb2cef9a93279979a90ed67dd1bb0d4dfff
Pedro Lacerda <pedrolacerda804@gmail.com>
2023-09-04 16:54:23 -0300

    Adds accordion list

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function initTabNav () {
 }
 
 initTabNav();
+
+function initAccordionFaq () {
+
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'active';
+
+    if (accordionList) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+    
+        function activeAccordion (event) {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        })
+    }
+
+}
+
+initAccordionFaq();
