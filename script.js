a8a1039f24e931b1e36837ff3751a3b8128956b0
Pedro Lacerda <pedrolacerda804@gmail.com>
2023-09-04 13:42:29 -0300

    The function and event will only occur when tabMenu and tabContent have content. (TRUE)

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const tabMenu = document.querySelectorAll('.js-tab-menu li');
 const tabContent = document.querySelectorAll('.js-tab-content section');
 
-tabContent[0].classList.add('active');
+if (tabMenu && tabContent) {
+    tabContent[0].classList.add('active');
 
-function activeTab (index) {
-    // Remove all class like "active"
-    tabContent.forEach((section) => {
-        section.classList.remove('active');
+    function activeTab (index) {
+        // Remove all class like "active"
+        tabContent.forEach((section) => {
+            section.classList.remove('active');
+        })
+    
+        // Add the "active" class to the respective clicked index
+        tabContent[index].classList.add('active');
+    }
+    
+    tabMenu.forEach((itemMenu, index) => {
+        // We create an arrow function with callback in the event and pass the index stored in forEach
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
     })
-
-    // Add the "active" class to the respective clicked index
-    tabContent[index].classList.add('active');
 }
-
-tabMenu.forEach((itemMenu, index) => {
-    // We create an arrow function with callback in the event and pass the index stored in forEach
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    });
-})
